453d3698afb5c97820f5d526fc5d8d292b4ccdb2
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-12 14:46:40 -0700

    Tests broke AF, used to be good, now shit, Logic works through
    Co-authored-by: Filmer Tolentino <filmer.tolentino@gmail.com>
    Co-authored-by: Joseph Jack <Josephwjack@users.noreply.github.com>

--- TriangleTracker/Models/TriangleLogic.cs
+++ TriangleTracker/Models/TriangleLogic.cs
@@ -1,47 +1,46 @@
 using System;
 
-namespace TriangleTracker.Models {
-  
+namespace TriangleTracker
+{
   public class TriangleLogic
   {
-
-    public int Num1 { get; set; }
-    public int Num2 { get; set; }
-    public int Num3 { get; set; }
-
-    public TriangleLogic(int num1, int num2, int num3)
+    public static void Main()
     {
-      Num1 = num1;
-      Num2 = num2;
-      Num3 = num3;
+      
+
+      Console.WriteLine("Enter Triangle Side 1");
+      int num1 = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter Triangle Side 2");
+      int num2 = int.Parse(Console.ReadLine());
+      Console.WriteLine("Enter Triangle Side 3");
+      int num3 = int.Parse(Console.ReadLine());
     }
-
-    public static void IsTriangle(int num1, int num2, int num3)
-    {
-      if ((num1 + num2 <= num3 || num1 + num3 <= num2 || num2 + num3 <= num1) || (num1 + num2 >= num3 || num1 + num3 >= num2 || num2 + num3 >= num1))
+      public static void isTriangle(int num1, int num2, int num3)
       {
-        Console.WriteLine("This is not a triangle");
-      }
-      // check if all sides are equal
-      else if (num1 == num2 && num1 == num3 && num2 == num3)
-      {
-        Console.WriteLine("Equilateral");
-      }
-      // check if isoceles
-      else if (num1 == num2 || num1 == num3 || num2 == num3)
-      {
-        Console.WriteLine("Isosceles");
-      }
-      // check if no sides are equal 
-      else if (num1 != num2 && num1 != num3 && num2 != num3)
-      {
-        Console.WriteLine("Scalene");
-      }
-      // check if not a triangle
-      else
-      {
-        Console.WriteLine("Impossible");
+        if (num1 + num2 <= num3 || num1 + num3 <= num2 || num2 + num3 <= num1)
+        {
+          Console.WriteLine("This is not a triangle");
+        }
+        // check if all sides are equal
+        else if (num1 == num2 && num1 == num3 && num2 == num3)
+        {
+          Console.WriteLine("Equilateral");
+        }
+        // check if isoceles
+        else if (num1 == num2 || num1 == num3 || num2 == num3)
+        {
+          Console.WriteLine("Isosceles");
+        }
+        // check if no sides are equal 
+        else if (num1 != num2 && num1 != num3 && num2 != num3)
+        {
+          Console.WriteLine("Scalene");
+        }
+        // check if not a triangle
+        else
+        {
+          Console.WriteLine("Impossible");
+        }
       }
     }
   }
-}
