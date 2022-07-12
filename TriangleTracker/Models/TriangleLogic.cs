503f6b2af9e70eda688d2f64c36b511e3b7b893e
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-12 14:16:40 -0700

    WIP commit before refactoring to one file
    Co-authored-by: Filmer Tolentino <filmer.tolentino@gmail.com>
    Co-authored-by: Joseph Jack <Josephwjack@users.noreply.github.com>

--- TriangleTracker/Models/TriangleLogic.cs
+++ TriangleTracker/Models/TriangleLogic.cs
@@ -1,32 +1,46 @@
-namespace TriangleTracker.Models
-{
+using System;
+
+namespace TriangleTracker.Models {
+  
   public class TriangleLogic
   {
-    public bool IsTriangle(int num1, int num2, int num3)
+
+    public int Num1 { get; set; }
+    public int Num2 { get; set; }
+    public int Num3 { get; set; }
+
+    public TriangleLogic(int num1, int num2, int num3)
+    {
+      Num1 = num1;
+      Num2 = num2;
+      Num3 = num3;
+    }
+
+    public static void IsTriangle(int num1, int num2, int num3)
     {
       if ((num1 + num2 <= num3 || num1 + num3 <= num2 || num2 + num3 <= num1) || (num1 + num2 >= num3 || num1 + num3 >= num2 || num2 + num3 >= num1))
       {
-        return true;
+        Console.WriteLine("This is not a triangle");
       }
       // check if all sides are equal
       else if (num1 == num2 && num1 == num3 && num2 == num3)
       {
-        return true;
+        Console.WriteLine("Equilateral");
       }
       // check if isoceles
       else if (num1 == num2 || num1 == num3 || num2 == num3)
       {
-        return true;
+        Console.WriteLine("Isosceles");
       }
       // check if no sides are equal 
       else if (num1 != num2 && num1 != num3 && num2 != num3)
       {
-        return true;
+        Console.WriteLine("Scalene");
       }
       // check if not a triangle
       else
       {
-        return false;
+        Console.WriteLine("Impossible");
       }
     }
   }
